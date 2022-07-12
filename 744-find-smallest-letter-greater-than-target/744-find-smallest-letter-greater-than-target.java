93d23d8946af5130f347f0f34d75ce3132be86a0
Manish Bhardwaj <75626814+Manishh444@users.noreply.github.com>
2022-07-12 10:39:45 +0530

    Time: 1 ms (53.19%), Space: 48 MB (54.90%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -5,6 +5,8 @@ class Solution {
 
         while(start<= end){
             int mid = start + (end-start)/2;
+            
+        // if condition is very specific to this problem, in case if were to find the greatest element smaller than target elemnet then we would use this if condition for start paramenter and keep end paramenter in else conditiom    
              if( target < letters[mid]){
                 // start = mid +1;
                    end = mid-1;
@@ -15,6 +17,8 @@ class Solution {
                 start = mid +1;
             }
         }
+       // if target is greater than the greatest element in array than start will be equla to lenght of array and hence below logic will return reminder = 0; 
+        //and arr(0) elemt is returned
         return letters[start % letters.length];
     }
 }
